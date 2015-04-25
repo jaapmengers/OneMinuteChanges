fc2f77d2fcddea53899cbb83bfb6661753fc3f6d
Jaap Mengers <jaapm@q42.nl>
2015-04-25 14:24:00 +0200

    Count peaks

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # OneMinuteChanges
+During development, run with `sbt ~fastOptJS` (or `sbt "~fastOptJS"` if you use zsh), and open the app up in your browser, e.g: `http://localhost:12345/target/scala-2.11/classes/index.html`
--- src/main/scala/oneminutechanges/Main.scala
+++ src/main/scala/oneminutechanges/Main.scala
@@ -1,6 +1,7 @@
 package oneminutechanges
 
 import monifu.reactive.channels.PublishChannel
+import monifu.reactive._
 import monifu.concurrent.Implicits.globalScheduler
 
 import scala.collection.mutable
@@ -11,13 +12,43 @@ import scala.scalajs.js.annotation.JSExport
 @JSExport
 object Main extends JSApp{
 
+  trait VolumeLevel
+  case object Peak extends VolumeLevel
+  case object Baseline extends VolumeLevel
+
   val channel = PublishChannel[Double]()
 
   @JSExport
   def main(): Unit = {
 
-    channel.scan
+    val averages = channel.scan((Nil, 0.0): Tuple2[List[Double], Double] ) { (acc, cur) =>
+      val newList = acc._1 :+ cur
+      (newList, newList.sum / newList.length)
+    }
 
+    var peakCounter = 0
+    var isPeak = false
+
+    val buffered  = channel
+      .buffer(10)
+      .map(x => x.sum / x.length)
+      .combineLatest(averages)
+      .map { x =>
+        val curValue = x._1
+        val avg = x._2._2
+
+        if((curValue / avg) * 100 > 150)
+          Peak
+        else
+          Baseline
+      }: Observable[VolumeLevel]
+
+    val peaks = buffered.distinctUntilChanged.filter(x => x match {
+      case Peak => true
+      case Baseline => false
+    })
+
+    peaks.foreach(println)
   }
 
   @JSExport
